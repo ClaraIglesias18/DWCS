b012dede98f98d8a1f1cd3977811c08fcd76e068
ClaraIglesias18 <claraiglesias18@hotmail.com>
2022-10-28 18:48:56 +0200

    28/10

---
+++ cookies/adivinaNumero.php
@@ -0,0 +1,49 @@
+<?php
+$intentos = 5;
+$salida = "Te quedan ".$intentos. " intentos";
+
+if(!isset($_COOKIE['numAl'])) {
+    setcookie('numAl', random_int(0, 100), time() + 3600);
+}
+
+if(!isset($_COOKIE['intentos'])) {
+    setcookie('intentos', $intentos, time() + 3600);
+} else {
+    $intentos = $_COOKIE['intentos'];
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if($_POST['numero'] > $_COOKIE['numAl']) {
+        $salida = "El numero introducido es mayor";
+    } else if($_POST['numero'] < $_COOKIE['numAl']) {
+        $salida = "El numero introducido es menor";
+    } else {
+        $salida = "Enhorabuena has acertado!";
+    }
+
+    $intentos--;
+    $salida += " Intentos restantes: " . $intentos;
+    setcookie('intentos', $intentos, time() + 3600); 
+} else if($intentos == 0){
+    $salida = "No te quedan mas intentos, el numero era: " + $_COOKIE['numAl'];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adivina numero</title>
+</head>
+<body>
+    <h1>Adivina el numero</h1>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <input type="number" name="numero" id="numero">
+        <input type="submit" name="Enviar">
+    </form>
+    <?=$salida?>
+
+</body>
+</html>
--- cookies/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if(!isset($_COOKIE["nombre"])){
-        setcookie("nombre", "clara", time() + 3600);
-    }
-    echo "Mi nombre es: ".$_COOKIE["nombre"]; 
-?>
